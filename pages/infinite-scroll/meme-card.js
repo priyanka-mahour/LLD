49dd3a1c47b470c25cb10e23f51cfd3ba8376bd6
prinyanka-mahour <priyankamahour996@gmail.com>
2026-01-17 01:43:11 +0530

    fix: type error

--- pages/infinite-scroll/meme-card.js
+++ pages/infinite-scroll/meme-card.js
@@ -1,16 +1,19 @@
-
 const MemeCard = ({ data }) => {
-    const { title, preview } = data
- 
-    return (
-        <>
-            <div style={{ border: '1px solid grey', width: 'fit-content', padding: '1rem' }} key={title}>
-                <img src={preview[0]} alt='title'/>
-                <h2>{title}</h2>
-            </div>
-            <br/>
-        </>
-    )
-}
+  if (!data || !data.title || !data.preview) {
+    return null;
+  }
 
-export default MemeCard
+  const { title, preview } = data;
+
+  return (
+    <>
+      <div style={{ border: '1px solid grey', width: 'fit-content', padding: '1rem' }} key={title}>
+        <img src={preview[0]} alt={title} />
+        <h2>{title}</h2>
+      </div>
+      <br />
+    </>
+  );
+};
+
+export default MemeCard;
