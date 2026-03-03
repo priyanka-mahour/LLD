6569c1129900fcc168e9184cad057410b2dd1ac6
prinyanka-mahour <priyankamahour996@gmail.com>
2026-03-04 00:58:13 +0530

    fix yml

--- pages/redux/comp-to-pass-state/product.tsx
+++ pages/redux/comp-to-pass-state/product.tsx
@@ -3,50 +3,66 @@ import styles from './header.module.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
 import { fetchProducts } from '../redux-toolkit/product-slice'
-import type { AppDispatch } from '../redux-toolkit/store'
+import type { AppDispatch, RootState } from '../redux-toolkit/store'
+
+// Optional: type for each product item (update based on your API)
+interface ProductItem {
+  id: string
+  title: string
+  price: number
+  image: string
+}
 
 const Product = () => {
+    // type-safe dispatch
     const dispatch = useDispatch<AppDispatch>()
 
+    // fetch products on mount
     useEffect(() => {
         dispatch(fetchProducts())
-    }, [])
+    }, [dispatch])
 
-    const selector = useSelector(state => state.products.items)
+    // type-safe selector
+    const items = useSelector((state: RootState) => state.products.items)
 
     return (
         <section className={styles.products}>
-        <h2>Products</h2>
-        <button className={styles["add-btn"]} onClick={() => dispatch(clearAllItems())}>Clear Cart</button>
+            <h2>Products</h2>
 
-        <div className={styles["product-grid"]}>
+            <button
+                className={styles["add-btn"]}
+                onClick={() => dispatch(clearAllItems())}
+            >
+                Clear Cart
+            </button>
 
-        <div className={styles["product-card"]}>
-            <img src="https://media-ik.croma.com/prod/https://media.tatacroma.com/Croma%20Assets/Large%20Appliances/Washers%20and%20Dryers/Images/252060_0_EdXo48ipp.png?updatedAt=1764593518086?tr=w-1000" alt="Product 1" />
-            <h3>Product 1</h3>
-            <p>$10.00</p>
-            <button className={styles["add-btn"]} onClick={() => dispatch(addItem())}>Add to Cart</button>
-            <button className={`${styles["add-btn"]} ${styles["remove-btn"]}`} onClick={() => dispatch(removeItem())}>Remove from Cart</button>
-        </div>
+            <div className={styles["product-grid"]}>
 
-        <div className={styles["product-card"]}>
-            <img src="https://img-prd-pim.poorvika.com/cdn-cgi/image/width=1600,height=1600,quality=75/product/Whirlpool-8-0kg-fully-automatic-front-load-washing-machine-xpert-care-majestic-silver-Front-View.png" alt="Product 2" />
-            <h3>Product 2</h3>
-            <p>$15.00</p>
-            <button className={styles["add-btn"]} onClick={() => dispatch(addItem())}>Add to Cart</button>
-            <button className={`${styles["add-btn"]} ${styles["remove-btn"]}`} onClick={() => dispatch(removeItem())}>Remove from Cart</button>
-        </div>
+                {items?.map((item: ProductItem) => (
+                    <div className={styles["product-card"]} key={item.id}>
+                        <img src={item.image} alt={item.title} />
+                        <h3>{item.title}</h3>
+                        <p>${item.price}</p>
 
-        <div className={styles["product-card"]}>
-            <img src="https://img-prd-pim.poorvika.com/cdn-cgi/image/width=1600,height=1600,quality=75/product/Bosch-11-0kg-fully-automatic-front-load-washing-machine-series-6-woi115b0in-dark-lake-Front-Left-View.png" alt="Product 3" />
-            <h3>Product 3</h3>
-            <p>$20.00</p>
-            <button className={styles["add-btn"]} onClick={() => dispatch(addItem())}>Add to Cart</button>
-            <button className={`${styles["add-btn"]} ${styles["remove-btn"]}`} onClick={() => dispatch(removeItem())}>Remove from Cart</button>
-        </div>
-        </div>
+                        <button
+                            className={styles["add-btn"]}
+                            onClick={() => dispatch(addItem())}
+                        >
+                            Add to Cart
+                        </button>
+
+                        <button
+                            className={`${styles["add-btn"]} ${styles["remove-btn"]}`}
+                            onClick={() => dispatch(removeItem())}
+                        >
+                            Remove from Cart
+                        </button>
+                    </div>
+                ))}
+
+            </div>
         </section>
     )
 }
 
-export default Product;
+export default Product
